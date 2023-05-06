9a591928104d2568b53ced217d4ec119d5f5b93d
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2023-05-06 22:05:10 +0530

    added table view and bottom view with constraints

--- Stocks/ViewController.swift
+++ Stocks/ViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let stocksTableView = UITableView()
+    let aggregateView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        stocksTableView.backgroundColor = .red
+        aggregateView.backgroundColor = .green
         // Do any additional setup after loading the view.
+        view.addSubview(stocksTableView)
+        view.addSubview(aggregateView)
+        setConstraints()
+        getData()
+    }
+    
+    func getData() {
         NetworkManager().startLoad { result in
             switch result {
             case .success(let data):
@@ -20,6 +32,22 @@ class ViewController: UIViewController {
                 print("error : \(error.localizedDescription)")
             }
         }
+    }
+    
+    func setConstraints() {
+        stocksTableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        stocksTableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        stocksTableView.leftAnchor.constraint(equalTo:view.leftAnchor).isActive = true
+        stocksTableView.rightAnchor.constraint(equalTo:view.rightAnchor).isActive = true
+        stocksTableView.bottomAnchor.constraint(equalTo:view.bottomAnchor, constant: -200).isActive = true
+        
+        aggregateView.translatesAutoresizingMaskIntoConstraints = false
+        aggregateView.topAnchor.constraint(equalTo: stocksTableView.bottomAnchor).isActive = true
+        aggregateView.leftAnchor.constraint(equalTo:view.leftAnchor).isActive = true
+        aggregateView.rightAnchor.constraint(equalTo:view.rightAnchor).isActive = true
+        aggregateView.bottomAnchor.constraint(equalTo:view.bottomAnchor).isActive = true
+        
     }
 
 
